c17e8763bbcfd394299a5984ec73ae9ad0922a68
tKanchev <t.kanchev@yahoo.com>
2021-05-22 23:47:14 +0300

    Add Db project, models, context, cofigurations

--- BgFishingPlaces.Api/Startup.cs
+++ BgFishingPlaces.Api/Startup.cs
@@ -1,5 +1,7 @@
+using BgFishingPlaces.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,9 @@ namespace BgFishingPlaces.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddDbContext<BgFishingPlacesDbContext>(options => options
+                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ BgFishingPlaces.Database/Configuration/RoleConfig.cs
@@ -0,0 +1,25 @@
+﻿using BgFishingPlaces.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace CenturionVoting.Database.Configurations
+{
+    public class RoleConfig : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder
+                .Property(x => x.Name)
+                .IsRequired();
+
+            builder
+                .HasData(
+                    new Role
+                    {
+                        Id = Guid.Parse("9f4d859f-74fc-49e2-8b7e-7040fba11d55"),
+                        Name = "Admin",
+                    });
+        }
+    }
+}
